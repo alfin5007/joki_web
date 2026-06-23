40ef798bf345dfe8f48e173bede62e59cd266999
wahyu perdana <mmwahyuperdana99@gmmial.com>
2026-06-23 14:57:04 +0700

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import {
     defineConfig
 } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
@@ -12,12 +11,6 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/passkeys.js',
-            ],
-            refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
             ],
         }),
         tailwindcss(),
